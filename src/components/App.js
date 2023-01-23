34d10d649d241cf31c7362e119ba69bd7d98a82e
ramdanisyaputra <rmdn.syaputra@gmail.com>
2023-01-23 17:08:54 +0700

    state react

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,22 @@
 import React from "react";
-import Header from "./Header";
-import List from "./List";
+// import Header from "./Header";
+// import List from "./List";
 
 class App extends React.Component {
+    state = {
+        books: ["Whose child is this", "Chicken Soup for the soul"]
+    }
     render(){
         return(
             <div>
-                <Header/>
-                <List/>
+                {/* <Header/>
+                <List/> */}
                 <h1>Heloo World</h1>
+                {this.state.books.map(item =>
+                    <div>
+                        <h3>{item}</h3>
+                    </div>
+                )}
             </div>
         )
     }
