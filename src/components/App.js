d444f9ce48aca2336157d9c2abf139cc55a5ec0b
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2023-01-28 18:52:38 +0700

    props react

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,18 @@
 import React from "react";
 // import Header from "./Header";
-// import List from "./List";
+import List from "./List";
 
 class App extends React.Component {
     state = {
-        books: ["Whose child is this", "Chicken Soup for the soul"]
+        book: "Chicken soup for the soul"
     }
     render(){
         return(
             <div>
-                {/* <Header/>
-                <List/> */}
-                <h1>Heloo World</h1>
-                {this.state.books.map((item, index) =>
-                    <div key={index}>
-                        <h3>{item}</h3>
-                    </div>
-                )}
+                <List name="kodeakademia" test="test" myBook={this.state.book}>
+                    <h4>This is title</h4>
+                    <p>oh my paragraph</p>
+                </List>
             </div>
         )
     }
--- src/components/List.js
+++ src/components/List.js
@@ -1,12 +1,14 @@
 import React from "react";
-import ListItem from "./ListItem";
+// import ListItem from "./ListItem";
 
 class List extends React.Component {
     render() {
         return (
             <div>
-                <h2>List Component</h2>
-                <ListItem/>
+                <h2>{ this.props.children }</h2>
+                <h3>{ this.props.name }</h3>
+                <h4>{ this.props.myBook }</h4>
+                <p>{ this.props.test }</p>
             </div>
         )
     }
