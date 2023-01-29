f3d3340b69cce9b02ab3a6aee2de54d0621d8dd3
Ramdani Syaputra <rmdn.syaputra@gmail.com>
2023-01-29 07:47:43 +0700

    event and set state

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 // import Header from "./Header";
 // import List from "./List";
-import Button from "./Button";
+// import Button from "./Button";
 
 class App extends React.Component {
+    state = {
+        name: "kodeakademia"
+    }
+    change = () => {
+        this.setState({
+            name: "kode"
+        })
+    }
     render(){
         return(
             <div>
-                <Button color="biru"/>
+                <h1>{this.state.name}</h1>
+                <button onClick={this.change}>Click</button>
             </div>
         )
     }
